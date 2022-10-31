fe9b79d77bd18a0a51e7bd9cabe74ff3896e5030
Dustin McMullen <mcmullendustin@yahoo.com>
2022-10-31 00:46:14 -0500

    Added authentication & context

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,7 +1,39 @@
+import { useContext, useState } from 'react';
+import { Alert } from 'react-native';
+
 import AuthContent from '../components/Auth/AuthContent';
+import {signin} from "../util/auth.js";
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { AuthContext } from '../store/auth-context';
 
 function LoginScreen() {
-  return <AuthContent isLogin />;
+
+  const authCtx = useContext(AuthContext);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function loginHandler ({email, password}) {
+    setIsLoading(true);
+    try {
+      const token = await signin(email, password);
+      authCtx.authenticate(token);
+    } catch(error) {
+      Alert.alert("Authentication failed", "Could not log you in, please check your credentials.")
+    }
+    
+    setIsLoading(false);
+  }
+
+  if (isLoading) {
+    return <LoadingOverlay
+      message="Loggin In..."
+    />
+  }
+
+  return <AuthContent
+    isLogin
+    onAuthenticate={loginHandler}
+  />;
 }
 
 export default LoginScreen;
--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,7 +1,37 @@
+import { useContext, useState } from 'react';
+import { Alert } from 'react-native';
+
 import AuthContent from '../components/Auth/AuthContent';
+import {createUser} from "../util/auth.js";
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { AuthContext } from '../store/auth-context';
 
 function SignupScreen() {
-  return <AuthContent />;
+
+  const authCtx = useContext(AuthContext);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function signupHandler({email, password}) {
+    setIsLoading(true);
+    try {
+      const token = await createUser(email, password);
+      authCtx.authenticate(token);
+    } catch(error) {
+      Alert.alert("Could not create user", "Please check your inputs or try again later.")
+    }
+    setIsLoading(false);
+  }
+
+  if (isLoading) {
+    return <LoadingOverlay
+      message="Creating User..."
+    />
+  }
+
+  return <AuthContent
+    onAuthenticate={signupHandler}
+  />;
 }
 
 export default SignupScreen;
